949f2f7d31415437f5615e75dd03f6b24b46c2cc
Luiza Oliveira <luiza8.marlene@gmai.com>
2023-08-24 16:51:49 -0300

    Temas

---
+++ src/global/theme.ts
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    background: '#fff',
+    text: '#050617',
+    light_text: 'rgba(23, 23, 27, 0.6)',
+    detail: '#747476',
+    green: '#00d172',
+    red: '#ff3b30',
+  
+  },
+};
