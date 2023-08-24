e03bdf2512c1874ca5aee4faa679b957521e80c1
Luiza Oliveira <luiza8.marlene@gmai.com>
2023-08-24 19:50:18 -0300

    alias do service

--- babel.config.js
+++ babel.config.js
@@ -7,6 +7,7 @@ module.exports = {
         alias: {
           // This needs to be mirrored in tsconfig.json
           screens: "./src/Screens",
+          services: "./src/Services",
         },
       },
     ],
