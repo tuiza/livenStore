ce51b8c599055dbb27e46907245e59893273e88b
Luiza Oliveira <luiza8.marlene@gmai.com>
2023-08-29 11:19:28 -0300

    padding

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {
+  Platform,
   SafeAreaView, StatusBar,
   useColorScheme
 } from 'react-native';
@@ -10,17 +11,27 @@ import { CartProvider } from 'contexts/ProductContext';
 import { FavoritesProvider } from 'contexts/FavoriteContext';
 
 function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark'
+  StatusBar.setBarStyle("light-content");
+  useEffect(() => {
+    if (Platform.OS === 'android') {
+      StatusBar.setBackgroundColor("rgba(0,0,0,0)");
+      StatusBar.setTranslucent(true);
+    }
+  }, [])
+
+  const padding = Platform.OS === "android" ? StatusBar.currentHeight : 0 
+
   return (
     <CartProvider>
       <FavoritesProvider>
-        <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.cinza }}>
+        <SafeAreaView style={{ flex: 1, backgroundColor: theme.colors.cinza, marginTop: padding }}>
           <StatusBar barStyle={'dark-content'} />
           <AppRoutes />
         </SafeAreaView>
       </FavoritesProvider>
     </CartProvider>
-  )
+)
+  
 }
 
 export default App;
